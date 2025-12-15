6d9e2f6ebeb5d9e02f2890b49873f5cb388bd5a5
Carlos <cgmf.soul@gmail.com>
2025-12-15 02:18:56 -0300

    Test de clientes cambiado por cambios en el modelo

--- tarjetagamificada/src/test/java/cl/inf331/tarjetagamificada/domain/ClienteTest.java
+++ tarjetagamificada/src/test/java/cl/inf331/tarjetagamificada/domain/ClienteTest.java
@@ -13,54 +13,22 @@ class ClienteTest {
     void setUp() {
         clienteValido = new Cliente("1", "Cliente", "cliente@correo.com");
     }
-
+    
     @Test
-    void clienteNuevo_iniciaConPuntosCero_nivelBronce_streakCero() {
-        assertEquals(0, clienteValido.getPuntos());
+    void clienteValidoConParametrosCorrectos() {
+    	assertEquals(0, clienteValido.getPuntos());
         assertEquals(Nivel.BRONCE, clienteValido.getNivel());
         assertEquals(0, clienteValido.getStreakDias());
-    }
-
-    @Test
-    void clienteNuevo_guardaIdNombreYCorreo() {
         assertEquals("1", clienteValido.getId());
-        assertEquals("Cliente", clienteValido.getNombre());
         assertEquals("cliente@correo.com", clienteValido.getCorreo());
+        assertEquals("Cliente", clienteValido.getNombre());
     }
-
+    
     @Test
     void noPermiteCorreoSinArroba() {
         assertThrows(IllegalArgumentException.class,
-                () -> new Cliente("2", "X", "correoInvalido"));
+                () -> new Cliente("2", "Otro", "correoInvalido"));
     }
-
-    @Test
-    void noPermiteCorreoNull() {
-        assertThrows(IllegalArgumentException.class,
-                () -> new Cliente("3", "X", null));
-    }
-
-    @Test
-    void permiteCorreoConArroba() {
-        Cliente c = new Cliente("4", "X", "x@y.com");
-        assertEquals("x@y.com", c.getCorreo());
-    }
-
-    @Test
-    void permiteActualizarNombre() {
-        clienteValido.setNombre("Nuevo Nombre");
-        assertEquals("Nuevo Nombre", clienteValido.getNombre());
-    }
-
-    @Test
-    void permiteActualizarCorreoSiEsValido() {
-        clienteValido.setCorreo("nuevo@correo.com");
-        assertEquals("nuevo@correo.com", clienteValido.getCorreo());
-    }
-
-    @Test
-    void noPermiteActualizarCorreoInvalido() {
-        assertThrows(IllegalArgumentException.class,
-                () -> clienteValido.setCorreo("sinArroba"));
-    }
+    
+    
 }
